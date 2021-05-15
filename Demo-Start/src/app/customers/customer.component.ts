f3f1abff33e97a2e11e250cfca90955fcbc26651
Fredy Jimenez <fredy.alberto.jimenez@pivotree.com>
2021-05-14 19:37:17 -0500

    Angular Reactive form

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,7 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators, ValidatorFn} from '@angular/forms';
 
-import { Customer } from './customer';
+import {Customer} from './customer';
+import {debounceTime} from "rxjs/operators";
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  let emailControl = c.get('email');
+  let confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  } else {
+    return {'match': true};
+  }
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean | null } => {
+    let value = c.value;
+    if (value !== null && (isNaN(value)) || value < 1 || value > 5) {
+      return {'range': true};
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +35,73 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+
+  customerForm: FormGroup;
   customer = new Customer();
+  emailMessage: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.'
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.customerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', [Validators.required]],
+      },{validator: emailMatcher}),
+      phone: '',
+      notification: 'email',
+      rating: [null, [ratingRange(1, 5)]],
+      sendCatalog: true
+    });
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(
+      debounceTime(1000)
+  ).subscribe(
+      value => this.setEmailMessage(emailControl)
+    );
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: "Harkness",
+      sendCatalog: false
+    });
+  }
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm));
+  }
+
+  setNotification(notifyVia: string): void {
+    let phoneControl = this.customerForm.get('phone');
+    if(notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required)
+    } else {
+      phoneControl.clearValidators()
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
+  setEmailMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 }
